8a777fe593f2c1362dabff784ea469b4aa0cd3a1
connor whalen <cjwhalen@uw.edu>
2024-07-12 12:18:03 -0500

    added packages for ggplot

--- REU/Jolee/JT Script.R
+++ REU/Jolee/JT Script.R
@@ -2,3 +2,15 @@
 ### created 7/11/24        ###
 ### by Jolee Thirtyacre    ###
 ### and Connor Whalen      ###
+
+# load packages
+install.packages("tidyverse")
+install.packages("janitor")
+install.packages("ggeffects")
+library(ggeffects)
+library(tidyverse)
+library(readr)
+library(dbplyr)
+library(ggarrange)
+library(lubridate)
+library(ggplot2)
