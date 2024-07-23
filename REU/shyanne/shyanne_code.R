5e2c7ec321d1c33e95aa9876179a16c42db2167e
Chelsea Wood <chelsealwood@gmail.com>
2024-07-23 09:48:01 -0500

    shyanne's second figure

--- REU/shyanne/shyanne_code.R
+++ REU/shyanne/shyanne_code.R
@@ -3,6 +3,17 @@
 # christner.shyanne03@gmail.com and chelwood@uw.edu
 # Started 4 July 2024
 
+
+# Load up the packagaes you need
+
+library(ggplot2)
+library(tidyverse)
+library(lme4)
+library(car)
+library(ggeffects)
+library(reshape2)
+
+
 # R is really useful for doing quick tallies and plots.
 # Let's do some together!
 
@@ -33,9 +44,12 @@ plot(ict_pun_data$MONO.IP~ict_pun_data$Latitude)+abline(v=30.76)
 
 summary(lm(pim_vig_data$MONO.IP~pim_vig_data$Latitude))
 
-### PRELIMINARY ANALYSIS - CHELSEA, 15 JULY 2024
 
-full_data<-read.csv("data/processed/Full_dataset_with_psite_life_history_info.csv", header = T, sep = ",")
+
+
+### PRELIMINARY ANALYSIS - CHELSEA, 22 JULY 2024
+
+full_data<-read.csv("data/processed/Full_dataset_with_psite_life_history_info_2024.07.19.csv", header = T, sep = ",")
 colnames(full_data)[20]<-"scaled_TL_mm"
 
 View(full_data)
@@ -68,6 +82,7 @@ vif(model_1)
 library(ggeffects)
 color_predict<-ggeffect(model_1,c("Life_History","CI"))
 
+library(ggplot2)
 color_plot<-ggplot(color_predict,aes(c(1.95,0.95,2.05,1.05),predicted),group=Life_History,linetype=Life_History)+
   geom_errorbar(data=color_predict,mapping=aes(x=c(1.95,0.95,2.05,1.05),ymin=conf.low,ymax=conf.high),width=0.03)+
   geom_line(aes(group=group,linetype=group))+
@@ -85,6 +100,7 @@ color_plot<-ggplot(color_predict,aes(c(1.95,0.95,2.05,1.05),predicted),group=Lif
   theme(legend.position="top",legend.title = element_text(size = 18),
         legend.text = element_text(size=14))
 color_plot
+
 
 
 
@@ -113,32 +129,34 @@ write.table(model_2_output,"REU/shyanne/model_2_output_2024.07.15")
 
 
 
-# NOTE: I'm still working on this plot - not yet up and running.
+# Now let's plot it
 
 library(ggeffects)
-color_predict<-ggeffect(model_2,c("Parasite_taxonomic_group","CI"))
+color_predict_2<-ggeffect(model_2,c("Parasite_taxonomic_group","CI"))
 
-# Need to exclude Hirudinea because error bars are whacky (probably because we found so few).
+# Need to exclude Hirudinea because error bars are wacky (probably because we found so few). For now, I've
+# just taken the error bars off.
 
-color_predict$group
+# Set up a nice color palette first.
+library(viridis)
+pal<-viridis(n=9)
 
-color_plot<-ggplot(color_predict,aes(group,predicted),group=Parasite_taxonomic_group,
-                   linetype=Parasite_taxonomic_group)+
-  geom_point(size=6,pch=21)+
-  geom_errorbar(data=color_predict,mapping=aes(x="CI",ymin=conf.low,ymax=conf.high),width=0.03)+
-  geom_line(aes(group=group,linetype=group))+
+color_plot_2<-ggplot(color_predict_2,aes(group,predicted),group=Parasite_taxonomic_group,
+                   color=Parasite_taxonomic_group)+
+  geom_point(aes(group=x,color=x),size=4,pch=19)+
+  #geom_errorbar(data=color_predict_2,mapping=aes(x="CI",ymin=conf.low,ymax=conf.high),width=0.03)+
+  geom_line(aes(group=x,color=x,linetype=x))+
+  scale_color_manual(name = c("parasite taxonomic group"),values=pal)+
   xlab("treatment")+
   ylab("predicted parasite abundance\n per parasite taxon per host individual")+
   theme_minimal()+
   #labs(linetype="parasite life history strategy")+
-  scale_linetype_discrete(name = c("parasite taxonomic group"),labels=c("Acanthocephala","Cestoda","Hirudinea",
-                                                                        "Monogenea","Myxozoa","Trematoda"))+
   theme(plot.title=element_text(size=18,hjust=0.5,face="plain"),axis.text.y=element_text(size=14),axis.title.y=element_text(size=16),
         axis.text.x=element_text(size=18,color=c("cadetblue","burlywood4")),axis.title.x=element_text(size=16),
         panel.background=element_rect(fill="white",color="black"),panel.grid.major=element_line(color=NA),
         panel.grid.minor=element_line(color=NA),plot.margin=unit(c(0,0,0,0),"cm"))+
-  scale_x_discrete(limits=rev(levels(color_predict$group)))+
-  theme(legend.position="top",legend.title = element_text(size = 18),
+  scale_x_discrete(limits=(levels(color_predict_2$group)))+
+  theme(legend.position="left",legend.title = element_text(size = 18),
         legend.text = element_text(size=14))
-color_plot
+color_plot_2
 
