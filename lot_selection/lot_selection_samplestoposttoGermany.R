ddcf3c63d05777acacd228ef1b20b70c0099f046
dakeishla <diazdakeishla@gmail.com>
2024-07-30 20:29:52 -0500

    Update code

--- lot_selection/lot_selection_samplestoposttoGermany.R
+++ lot_selection/lot_selection_samplestoposttoGermany.R
@@ -17,31 +17,52 @@ View(PIMVIG)
 str(PIMVIG)
 
 library(dplyr)
-PIMVIG %>% count(combo)
 
+# Summarize data by jar/lot
 PIMVIGb <- PIMVIG %>%
   group_by(CatalogNumber,combo)%>%
-summarize(CI = any(CI),.groups = 'drop')%>%
+summarize(CI = any(CI),.groups = 'drop')%>% #This does not work well as of now but gets the job done: having a list of jars from which 4 fish were dissected
   as.data.frame()
 
-View(PIMVIGb)
+#View(PIMVIGb)
 
+# Count how many jars were selected per combo
+PIMVIGb %>% count(combo)
+
+
+#Select random lots per combo
 LotPV_control_1954to1963<-PIMVIGb[ sample( PIMVIGb$CatalogNumber, which( PIMVIGb$combo == "control_1954-1963"), 1, replace = F), ]
-LotPV_control_1964to1973<-PIMVIG[ sample( which( PIMVIG$combo == "control_1964-1973"), 10, replace = F), ]
+LotPV_control_1964to1973<-PIMVIGb[ sample( which( PIMVIGb$combo == "control_1964-1973"), 10, replace = F), ]
 
-control_1974to1983<-PIMVIG[ sample( which( PIMVIG$combo == "control_1974-1983"), 3, replace = F), ]
-control_1984to1993<-PIMVIG[ sample( which( PIMVIG$combo == "control_1984-1993"), 1, replace = F), ]#not enough for 5
-control_1994to2003<-PIMVIG[ sample( which( PIMVIG$combo == "control_1994-2003"), 2, replace = F), ]#not enough for 5
-control_2004to2013<-PIMVIG[ sample( which( PIMVIG$combo == "control_2004-2013"), 2, replace = F), ]#not enough for 5
+LotPV_control_1974to1983<-PIMVIGb[ sample( which( PIMVIGb$combo == "control_1974-1983"), 3, replace = F), ]
+LotPV_control_1984to1993<-PIMVIGb[ sample( which( PIMVIGb$combo == "control_1984-1993"), 1, replace = F), ]#not enough for 5
+LotPV_control_1994to2003<-PIMVIGb[ sample( which( PIMVIGb$combo == "control_1994-2003"), 2, replace = F), ]#not enough for 5
+LotPV_control_2004to2013<-PIMVIGb[ sample( which( PIMVIGb$combo == "control_2004-2013"), 2, replace = F), ]#not enough for 5
+
+LotPV_impact_1954to1963<-PIMVIGb[ sample( which( PIMVIGb$combo == "impact_1954-1963"), 5, replace = F), ]
+LotPV_impact_1964to1973<-PIMVIGb[ sample( which( PIMVIGb$combo == "impact_1964-1973"), 5, replace = F), ]
+LotPV_impact_1974to1983<-PIMVIGb[ sample( which( PIMVIGb$combo == "impact_1974-1983"), 5, replace = F), ]
+LotPV_impact_1984to1993<-PIMVIGb[ sample( which( PIMVIGb$combo == "impact_1984-1993"), 5, replace = F), ]
+LotPV_impact_1994to2003<-PIMVIGb[ sample( which( PIMVIGb$combo == "impact_1994-2003"), 5, replace = F), ]
+LotPV_impact_2004to2013<-PIMVIGb[ sample( which( PIMVIGb$combo == "impact_2004-2013"), 4, replace = F), ]#don't bother because we don't have this decade for control
 
 
-impact_1954to1963<-PIMVIG[ sample( which( PIMVIG$combo == "impact_1954-1963"), 5, replace = F), ]
-impact_1964to1973<-PIMVIG[ sample( which( PIMVIG$combo == "impact_1964-1973"), 5, replace = F), ]
-impact_1974to1983<-PIMVIG[ sample( which( PIMVIG$combo == "impact_1974-1983"), 5, replace = F), ]
-impact_1984to1993<-PIMVIG[ sample( which( PIMVIG$combo == "impact_1984-1993"), 5, replace = F), ]
-impact_1994to2003<-PIMVIG[ sample( which( PIMVIG$combo == "impact_1994-2003"), 5, replace = F), ]
-impact_2004to2013<-PIMVIG[ sample( which( PIMVIG$combo == "impact_2004-2013"), 4, replace = F), ]#don't bother because we don't have this decade for control
+# Put data together
+PIMVIG_postDE <-rbind.data.frame(LotPV_control_1954to1963,
+                                 LotPV_control_1964to1973,
+                                 LotPV_control_1974to1983,
+                                 LotPV_control_1984to1993,
+                                 LotPV_control_1994to2003,
+                                 LotPV_control_2004to2013,
+                                 LotPV_impact_1954to1963,
+                                 LotPV_impact_1964to1973,
+                                 LotPV_impact_1974to1983,
+                                 LotPV_impact_1984to1993,
+                                 LotPV_impact_1994to2003,
+                                 LotPV_impact_2004to2013)
 
+# Export table 
+write.csv(per_vig_matrix,file="lot_selection/PIMVIG_postDE.csv")
 
 
 
