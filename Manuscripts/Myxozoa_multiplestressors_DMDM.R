b16868d0aa4c8345da270f7d676ff2b37e385388
dakeishla <diazdakeishla@gmail.com>
2024-08-17 19:26:18 -0500

    more to the code...

--- Manuscripts/Myxozoa_multiplestressors_DMDM.R
+++ Manuscripts/Myxozoa_multiplestressors_DMDM.R
@@ -6,6 +6,28 @@
 
 ### Preparation of data frames----
 
+##Install packages
+
+library(readxl)
+library(stats)
+library(DHARMa)
+library(MASS)
+library(sjPlot)
+library(ggplot2)
+library(ggeffects)
+library(sjmisc)
+library(sjlabelled)
+library(lme4)
+library(investr)
+library(ggsci)
+library(ggpubr)
+library(multcomp)
+library(effects)
+library(glmmTMB)
+library(itsadug)
+library(mgcv)
+
+
 ## Import data
 
 full_dataset <- read_csv("data/processed/Full_dataset_with_psite_life_history_info_2024.08.17.csv")
@@ -18,12 +40,18 @@ full_dataset_myxo <- subset(full_dataset, Parasite_taxonomic_group == "Myxozoa")
 
 full_dataset_myxo$Parasite_genus <- as.factor(full_dataset_myxo$Parasite_genus)
 
+## Create column for presence and absence of myxozoans
+full_dataset_myxo$psite_presence <- ifelse(full_dataset_myxo$psite_count > 0,                # condition
+                                               1,    # what if condition is TRUE
+                                               0)       # what if condition is FALSE
+
+## Remove columns with myxozoans that aren't confirmed to be myxozoans and therefore are not identified
+full_dataset_myxo <- subset(full_dataset_myxo, !is.na(Parasite_genus))
 
 
 ## Make a subset for data before the clean water act
 
 full_dataset_myxo_bca <- subset(full_dataset_myxo, YearCollected < 1973)
-
 
 
 ### Before clean water act----
@@ -44,5 +72,39 @@ ggplot(full_dataset_myxo_bca,aes(Latitude,psite_count, color = Parasite_genus,gr
   facet_wrap("Fish_sp.x")+
   geom_vline(xintercept=30.76, linetype="dashed", color = "black", size=0.5)+
   apatheme
+
+
+# GLM for the probability of finding myxozoans along the river
+glm_presence<-glm(psite_presence ~ poly(Latitude,2),
+             data = full_dataset_myxo_bca,family=binomial())
+
+glm_presence<-glmmTMB(psite_presence ~ poly(Latitude,2)*Parasite_genus+
+                        +(1|CatalogNumber),
+                  data = full_dataset_myxo_bca,family=binomial())
+
+
+summary(glm_presence)
+
+#Evaluate residuals
+#Not suitable for GLM-quasipoisson but for the other models
+s=simulateResiduals(fittedModel=glm_presence,n=250)
+s$scaledResiduals
+plot(s)
+
+
+#With the plot()function
+mydf <- ggpredict(glm_presence, c("Latitude")) 
+
+apatheme= theme_bw(base_size = 11,base_family = "sans")+
+  theme(panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        panel.border=element_blank(),
+        axis.line=element_line())
+
+plot(mydf,rawdata=TRUE)+
+  labs(x = 'Latitude', y = 'Probability of myxozoan presence (%)',title=NULL)+
+  apatheme+
+  geom_vline(xintercept=30.76, linetype="dashed", color = "black", size=0.5)
+  
 
 
